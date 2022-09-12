7e03efee36c34e88b7afeed41dea4c3dcf3a68af
DenerBarros <denerbarros.eng@gmail.com>
2022-09-12 15:39:35 -0300

    Criado as funções e mudanças no layout conforme o figma

--- js/favorites.js
+++ js/favorites.js
@@ -1,25 +1,141 @@
+import { GithubUser } from "./GithubUser.js";
+
 export class Favorites {
-    constructor(root){
-        this.root = document.querySelector(root);
+  constructor(root) {
+    this.root = document.querySelector(root);
+    this.load();
+  }
+
+  load() {
+    this.entries = JSON.parse(localStorage.getItem("@github-favorites:")) || [];
+  }
+
+  save() {
+    localStorage.setItem("@github-favorites:", JSON.stringify(this.entries));
+  }
+
+  async add(username) {
+    try {
+      const userExists = this.entries.find((entry) => entry.login === username);
+
+      if (userExists) {
+        throw new Error("Usuário já cadastrado");
+      }
+
+      const user = await GithubUser.search(username);
+
+      if (user.login === undefined) {
+        throw new Error("Usuário não encontrado");
+      }
+
+      this.entries = [user, ...this.entries];
+      this.update();
+      this.save();
+    } catch (error) {
+      alert(error.message);
     }
+  }
+
+  delete(user) {
+    const filteredEntries = this.entries.filter(
+      (entry) => entry.login !== user.login
+    );
+
+    this.entries = filteredEntries;
+
+    const noFav = this.entries.length === 0;
+
+    this.update();
+    this.save();
+  }
 }
 
 export class FavoritesView extends Favorites {
-    constructor(root) {
-        super(root)
+  constructor(root) {
+    super(root);
 
-        this.update();
-    }
+    this.tbody = this.root.querySelector("table tbody");
 
-    update() {
-      this.removeAllTr();
-    }
+    this.update();
+    this.onadd();
+  }
 
-    removeAllTr() {
-        const tbody = this.root.querySelector('table tbody');
-        tbody.querySelectorAll('tr').forEeach(() => {
-            tr.remove()
+  onadd() {
+    const addButton = this.root.querySelector(".search button");
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector(".search input");
 
-        })
-    }
+      this.add(value);
+    };
+  }
+
+  emptyFavorite() {
+    this.entries.length === 0
+      ? document.querySelector(".empty-favorite").classList.remove("sr-only")
+      : document.querySelector(".empty-favorite").classList.add("sr-only");
+  }
+
+  update() {
+    this.emptyFavorite();
+
+    this.removeAllTr();
+
+    this.entries.forEach((user) => {
+      const row = this.createRow();
+
+      row.querySelector(
+        ".user img"
+      ).src = `https://github.com/${user.login}.png`;
+
+      row.querySelector(".user img").alt = `Imagem de ${user.name}`;
+
+      row.querySelector(".user a").href = `https://github.com/${user.login}`;
+
+      row.querySelector(".user p").textContent = user.name;
+
+      row.querySelector(".user span").textContent = user.login;
+
+      row.querySelector(".repositories").textContent = user.public_repos;
+
+      row.querySelector(".followers").textContent = user.followers;
+
+      row.querySelector(".remove").onclick = () => {
+        const isOk = confirm("Deseja remover este usuário?");
+        if (isOk) {
+          this.delete(user);
+        }
+      };
+
+      this.tbody.append(row);
+    });
+  }
+
+  createRow() {
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = ` 
+    
+    <td class="user"><img src="https://github.com/maykbrito.png" alt="">
+					<a href="https://github.com/maykbrito" target="_blank">
+						<p>Mayk Brito</p>
+						<span>/maykbrito</span>
+					</a>
+				
+				</td>
+				<td class="repositories"></td>
+				<td class="followers"></td>
+				<td>
+					<button class="remove">Remover
+					</button>
+				</td>
+  `;
+
+    return tr;
+  }
+
+  removeAllTr() {
+    this.tbody.querySelectorAll("tr").forEach((tr) => {
+      tr.remove();
+    });
+  }
 }
