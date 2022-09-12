ba34fd7fb8fde00ab55cb17a884ef0f04db9ce55
DenerBarros <denerbarros.eng@gmail.com>
2022-09-12 14:46:27 -0300

    Criado o início da lógica em Javascript

---
+++ js/favorites.js
@@ -0,0 +1,18 @@
+export class Favorites {
+    constructor(root){
+        this.root = document.querySelector(root);
+    }
+}
+
+export class FavoritesView extends Favorites {
+    constructor(root) {
+        super(root)
+
+        this.update();
+    }
+
+    update() {
+        const tbody = this.root.querySelector('table tbody');
+        tbody.querySelectorAll('tr');
+    }
+}
